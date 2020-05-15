ff161c20881f68a1b6d7c089476258d096443042
soni.niket29@gmail.com <nikz@0133>
2020-05-15 22:06:34 +0530

    Updates

---
+++ precache-manifest.a7fff9d4afa48245db6f135d00dcd79d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de75fe9e2c2de6078279cea7a20ab8ae",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "f239742cfc3a79e45e5a",
+    "url": "/monstors-rolodex/static/css/main.18328111.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monstors-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f239742cfc3a79e45e5a",
+    "url": "/monstors-rolodex/static/js/main.89f53ce8.chunk.js"
+  },
+  {
+    "revision": "d674ca77413385f3cc74",
+    "url": "/monstors-rolodex/static/js/runtime-main.03f1c085.js"
+  }
+]);
